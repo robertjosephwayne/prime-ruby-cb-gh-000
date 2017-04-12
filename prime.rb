e804e7d9f90bd385a978263421a5d30cfe55fb6b
Robert Wayne <robertjwayne@ucla.edu>
2017-04-12 19:50:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+
+return false if number <= 1
+
+counter = 2
+while counter < number
+  return false if number % counter == 0
+  counter += 1
+end
+
+true
+end
